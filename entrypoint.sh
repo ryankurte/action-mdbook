ccda8de3585f7e4344fae0e1e316ab7e8ac73cbb
ryan <ryan@kurte.nz>
2019-11-26 15:58:39 +1300

    check for defined but empty options

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,13 @@
 
 ARGS=""
 
-if [ -n "$INPUT_OUTPUT_DIR" ]; then
+if [[ -n "$INPUT_OUTPUT_DIR" && "$INPUT_OUTPUT_DIR" != "" ]]; then
+  echo "Using output dir: $INPUT_OUTPUT_DIR"
   ARGS="$ARGS -d $INPUT_OUTPUT_DIR"
 fi
 
-if [ -n "$INPUT_INPUT_DIR" ]; then
+if [[ -n "$INPUT_INPUT_DIR" && "$INPUT_INPUT_DIR" != "" ]]; then
+  echo "Using input dir: $INPUT_INPUT_DIR"
   ARGS="$ARGS $INPUT_INPUT_DIR"
 fi
 
